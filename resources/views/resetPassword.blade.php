906f4a768fc17a0bdd186e991e9f47e0a086544c
Your Name <you@example.com>
2022-06-03 12:18:37 +0300

    adding reset password

---
+++ resources/views/resetPassword.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+<h1>{{$details['title']}}</h1>
+<p>{{$details['body']}}</p><br>
+<a href="{{$details['link']}}">The link below</a>
+<p>Thanks!</p>
+</body>
+</html>
