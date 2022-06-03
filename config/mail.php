84c9589dde1de009466d0ad4713f5b27fddd2294
Your Name <you@example.com>
2022-06-03 12:12:07 +0300

    adding reset password

--- config/mail.php
+++ config/mail.php
@@ -78,6 +78,13 @@ return [
             ],
         ],
     ],
+    'stream' => [
+        'ssl' => [
+            'allow_self_signed' => true,
+            'verify_peer' => false,
+            'verify_peer_name' => false
+        ],
+    ],
 
     /*
     |--------------------------------------------------------------------------
@@ -91,8 +98,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'yahavmdan@gmail.com'),
+        'name' => env('MAIL_FROM_NAME', 'Shop Manager'),
     ],
 
     /*
